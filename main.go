15eb8edffd743f0d9ba843071a8c056a4bdb7521
Bruno Vasconcelos <brunohgv2@gmail.com>
2021-03-15 21:49:59 -0300

    Add default command

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 	"os"
 	"sync"
@@ -11,28 +12,32 @@ import (
 func main() {
     arguments := os.Args[1:]
 
-	command := strings.Join(arguments, " ")
-
-	deviceIds := adb.GetDevices()
-	outputDir, err := os.Getwd()
-	if err != nil {
-		panic("Could not locate the current directory")
+	if len(arguments) == 0 {
+		fmt.Println("Adbatch is working!")
+		fmt.Println("Run an adb command using adbatch to broadcast it to all connected devices.")
+	} else {
+		command := strings.Join(arguments, " ")
+	
+		deviceIds := adb.GetDevices()
+		outputDir, err := os.Getwd()
+		if err != nil {
+			panic("Could not locate the current directory")
+		}
+	
+		var wg sync.WaitGroup
+		for _, deviceId := range deviceIds {
+			wg.Add(1)
+	
+			go func(deviceId string) {
+				defer wg.Done()
+				process := runner.Process{
+					Command:	command,
+					DeviceId:	deviceId,
+					FilePath:	outputDir,
+				}
+				process.Run()
+			}(deviceId)
+		}
+		wg.Wait()
 	}
-
-	var wg sync.WaitGroup
-	for _, deviceId := range deviceIds {
-		wg.Add(1)
-
-		go func(deviceId string) {
-			defer wg.Done()
-			process := runner.Process{
-				Command:	command,
-				DeviceId:	deviceId,
-				FilePath:	outputDir,
-			}
-			process.Run()
-		}(deviceId)
-	}
-
-	wg.Wait()
 }
